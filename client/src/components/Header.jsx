1bf73083580eef311fc4ab4db87d512fadb7d717
doruk.tekel <doruk.tekel@bilgeadam.com>
2024-06-17 23:25:04 +0300

    header login link created , profile page ui created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,22 @@
 import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLogout = () => {
+    // Logout işlemlerini burada gerçekleştir
+  };
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="bg-slate-50  ">
       <div className="flex justify-between  items-center max-w-7xl mx-auto p-3">
@@ -29,13 +43,36 @@ const Header = () => {
             <li>
               <Link to={"/about"}>About</Link>
             </li>
-            <li>
-              <Link to={"/login"}>
-                <button class="bg-transparent hover:bg-slate-500 text-slate-700 hover:text-white py-1 px-2 border border-slate-500 hover:border-transparent rounded flex items-center gap-1">
-                  Login
-                  <FiLogIn />
-                </button>
+            <li className="relative">
+              <Link to="/profile" onClick={toggleDropdown}>
+                {currentUser ? (
+                  <img
+                    src={currentUser.profilePicture}
+                    alt="avatar"
+                    className="w-10 h-10 rounded-full"
+                  />
+                ) : (
+                  <button className="bg-transparent hover:bg-slate-500 text-slate-700 hover:text-white py-1 px-2 border border-slate-500 hover:border-transparent rounded flex items-center gap-1">
+                    Login
+                    <FiLogIn />
+                  </button>
+                )}
               </Link>
+              {isOpen && currentUser && (
+                <div className="absolute mt-2 w-44 right-0  bg-white rounded-lg shadow-lg py-1 text-center flex flex-col items-center gap-1">
+                  <p className=" text-gray-800">
+                    Welcome <br /> {currentUser.username}
+                  </p>
+
+                  <hr className="w-full" />
+                  <button
+                    className="block w-full text-gray-800 text-center hover:bg-slate-700 hover:text-white"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </button>
+                </div>
+              )}
             </li>
           </ul>
         </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="max-w-md mx-auto">
+      <form className="flex flex-col text-center gap-2 my-2">
+        <h1 className="font-semibold text-xl">Profile</h1>
+        <img
+          src={currentUser.profilePicture}
+          alt="avatar"
+          className="w-40 h-40 rounded-full shadow-xl cursor-pointer self-center"
+        />
+        <input
+          type="text"
+          className="bg-slate-50 rounded-xl p-2"
+          name="text"
+          placeholder="username"
+        />
+        <input
+          type="email"
+          className="bg-slate-50 rounded-xl p-2"
+          name="email"
+          placeholder="email"
+        />
+        <input
+          type="password"
+          className="bg-slate-50 rounded-xl p-2"
+          name="password"
+          placeholder="password"
+        />
+        <button
+          type="submit"
+          className="bg-slate-700 text-white p-2 rounded-xl disabled:opacity-70 hover:opacity-75 hover:shadow-lg"
+        >
+          Update
+        </button>
+      </form>
+      <div>
+        <button
+          type="submit"
+          className="bg-red-700 text-white p-2 rounded-xl disabled:opacity-70 hover:opacity-75 hover:shadow-lg w-full"
+        >
+          Delete Account
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
